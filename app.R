97c6a3073ab0fde3469051a34aefe93afb86590b
Ethan Lee Marburger <elmarbur@ncsu.edu>
2024-11-01 15:18:14 -0400

    Connecting to shinyapp.io account

---
+++ app.R
@@ -0,0 +1,7 @@
+# Connecting to my shinyapps.io account
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='ethanmarburger-st558',
+                          token='C02F4F9052D2C20BAFE97D7A2C50802A',
+                          secret='S/mh/enoVcs31ME14peUCsh6u+TUcgkEqrU3F296')
+
+# successfully connected to my account
